db640da0fec033be48fb07d589b671358961bca1
Andrei Balan <andrei.balan98@gmail.com>
2026-03-20 19:12:53 +0200

    initial commit

---
+++ src/main/java/org/epi_assist/EPIAssist_v2/EpiAssistV2Application.java
@@ -0,0 +1,12 @@
+package org.epi_assist.EPIAssist_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpiAssistV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpiAssistV2Application.class, args);
+	}
+}
---
+++ src/test/java/org/epi_assist/EPIAssist_v2/EpiAssistV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.epi_assist.EPIAssist_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpiAssistV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
