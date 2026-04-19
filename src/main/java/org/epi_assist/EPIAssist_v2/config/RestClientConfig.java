0477c472ae57a651999c3cc504646ce72a64a482
Andrei Balan <andrei.balan98@gmail.com>
2026-04-20 00:51:02 +0300

    docker connect to python service

--- src/main/java/org/epi_assist/EPIAssist_v2/config/RestClientConfig.java
+++ src/main/java/org/epi_assist/EPIAssist_v2/config/RestClientConfig.java
@@ -1,5 +1,6 @@
 package org.epi_assist.EPIAssist_v2.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
@@ -8,10 +9,13 @@ import org.springframework.web.client.RestClient;
 @Configuration
 public class RestClientConfig {
 
+    @Value("${python.service.url:http://localhost:8000}")
+    private String pythonServiceUrl;
+
     @Bean
     public RestClient restClient() {
         return RestClient.builder()
-                .baseUrl("http://localhost:8000")
+                .baseUrl(pythonServiceUrl)
                 .requestFactory(new SimpleClientHttpRequestFactory())
                 .build();
     }
