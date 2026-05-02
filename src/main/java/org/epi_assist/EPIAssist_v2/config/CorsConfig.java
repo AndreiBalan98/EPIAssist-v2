672afa56f816a54c4d66ce6b1adc34634a9feef6
Andrei Balan <andrei.balan98@gmail.com>
2026-05-03 00:22:04 +0300

    cors for domain

--- src/main/java/org/epi_assist/EPIAssist_v2/config/CorsConfig.java
+++ src/main/java/org/epi_assist/EPIAssist_v2/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("https://*.vercel.app", "http://localhost:3000")
+                .allowedOriginPatterns("https://www.epi-assist.org", "https://epi-assist.org", "https://*.vercel.app", "http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .maxAge(3600);
